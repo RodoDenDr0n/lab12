494e655c127b41fc26a8f8040fb4b7028b69e925
RodoDenDr0n <vandzhuraandrew@gmail.com>
2022-12-26 16:10:32 +0200

    commit

---
+++ src/main/java/task2/TimedDocument.java
@@ -0,0 +1,21 @@
+package task2;
+
+import lombok.Getter;
+
+public class TimedDocument implements Document {
+    public Document document;
+    @Getter
+    public String gcsPath;
+    public TimedDocument(Document document) {
+        this.document = document;
+        this.gcsPath = ((SmartDocument) document).getGcsPath();
+    }
+
+    @Override
+    public String parse() {
+        long start = System.currentTimeMillis();
+        String result = document.parse();
+        long end = System.currentTimeMillis();
+        return result + "Parsing time: " + (end - start) + "ms.\n";
+    }
+}
