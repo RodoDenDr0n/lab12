78d580d2c923dd21b12da9c72447e236ff3a36b0
RodoDenDr0n <vandzhuraandrew@gmail.com>
2022-12-26 16:04:00 +0200

    commit

---
+++ src/main/java/task1/Handler20.java
@@ -0,0 +1,7 @@
+package task1;
+
+public class Handler20 extends Handler {
+    public Handler20(){
+        super(20);
+    }
+}
--- src/main/java/task1/Main.java
+++
@@ -1,7 +0,0 @@
-package task1;
-
-public class Main {
-    public static void main(String[] args) {
-        // https://www.youtube.com/watch?v=dQw4w9WgXcQ
-    }
-}
